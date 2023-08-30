70cc0ebbe5ffcd268feb87db9356377d092a4542
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2023-08-30 23:19:13 +0900

    Update 1012_유기농_배추.kt

--- somin/week1/1012_유기농_배추.kt
+++ somin/week1/1012_유기농_배추.kt
@@ -2,27 +2,28 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 private val reader = BufferedReader(InputStreamReader(System.`in`))
+lateinit var graph: Array<Array<Int>>
 
 fun main() {
-    val T = readln().toInt()
+    val T = reader.readLine().toInt()
     repeat(T) {
         val (width, height, nums) = reader.readLine().split(" ").map { it.toInt() }
+        graph = Array(height) { Array(width) { 0 } } // 세로 크기 height 가로 크기 width 2차원 배열
         getCabbageNums(width, height, nums)
     }
 }
 
-fun check(i: Int, j: Int, width: Int, height: Int, graph: Array<IntArray>) {
+fun check(i: Int, j: Int, width: Int, height: Int) {
     if (graph[i][j] == 1) {
         graph[i][j] = 0
-        if (j != 0) check(i, j - 1, width, height, graph)
-        if (i != 0) check(i - 1, j, width, height, graph)
-        if (j != width - 1) check(i, j + 1, width, height, graph)
-        if (i != height - 1) check(i + 1, j, width, height, graph)
+        if (j != 0) check(i, j - 1, width, height)
+        if (i != 0) check(i - 1, j, width, height)
+        if (j != width - 1) check(i, j + 1, width, height)
+        if (i != height - 1) check(i + 1, j, width, height)
     }
 }
 
 fun getCabbageNums(width: Int, height: Int, nums: Int) {
-    val graph = Array(height) { IntArray(width) } // 세로 크기 height 가로 크기 width 2차원 배열
     repeat(nums) {
         val (x, y) = reader.readLine().split(" ").map { it.toInt() }
         graph[y][x] = 1
@@ -33,8 +34,8 @@ fun getCabbageNums(width: Int, height: Int, nums: Int) {
         for (j in 0 until width) {
             if (graph[i][j] == 1) {
                 graph[i][j] = 0
-                if (i != height - 1) check(i + 1, j, width, height, graph)
-                if (j != width - 1) check(i, j + 1, width, height, graph)
+                if (i != height - 1) check(i + 1, j, width, height)
+                if (j != width - 1) check(i, j + 1, width, height)
                 answer += 1
             }
         }
